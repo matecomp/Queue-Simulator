2ffa85bf23210aaed4a92f561c9e3427865905d2
Mateus Ildefonso <matecompufrj@gmail.com>
2017-11-11 13:46:18 -0200

    Add test module and exp_server test

---
+++ test/test_exp_server.py
@@ -0,0 +1,36 @@
+# test_server.py
+
+# -*- coding: utf-8 -*-
+from servers.exp_server import Server
+import pytest
+
+init_dict = dict(zip(
+					["is_empty","mu","start_time","last_service"],
+					[True, 1.0, 0.0, 0.0]
+				))
+def test_init():
+	server = Server()
+	assert server.get_params() == init_dict
+
+def test_do_service():
+	server = Server()
+	time = server.do_service(0)
+	assert server.is_empty() == False
+
+def test_update_equal():
+	server = Server()
+	time = server.do_service(0)
+	server.update(time)
+	assert server.is_empty() == True
+
+def test_update_greather():
+	server = Server()
+	time = server.do_service(0)
+	server.update(time+1)
+	assert server.is_empty() == True
+
+def test_update_lower():
+	server = Server()
+	time = server.do_service(0)
+	server.update(time-1)
+	assert server.is_empty() == False
