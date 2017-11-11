a8b9b7746698451f6a3d8bf5350110bf623a20f1
Mateus Ildefonso <matecompufrj@gmail.com>
2017-11-11 17:07:53 -0200

    add update test

--- test/test_exp_queue.py
+++ test/test_exp_queue.py
@@ -6,14 +6,19 @@ import collections as clt
 import pytest
 
 init_dict = dict(zip(
-					["lambda","beta","deque"],
-					[1.0, 1.0, clt.deque()]
+					["lambda","beta","start_time", "deque"],
+					[1.0, 1.0, 0.0, clt.deque()]
 				))
 def test_init():
 	queue = Queue()
 	assert queue.get_params() == init_dict
 
-def test_add_client():
+def test_gets():
+	queue = Queue()
+	assert queue.get_len() == 0
+	assert queue.get_time() == 0.0
+
+def test_add_client_and_get_len():
 	queue = Queue()
 	queue.add_client()
 	assert queue.get_len() == 1
@@ -37,3 +42,16 @@ def test_pop_lcfs():
 	queue.add_client(10)
 	queue_temp = queue.get_params()["deque"]
 	assert queue_temp[-1] == queue.pop_lcfs()
+
+def test_update():
+	queue = Queue()
+	l1 = queue.get_len()
+	assert queue.get_time() == 0.0
+	queue.update(10.0)
+	l2 = queue.get_len()
+	assert queue.get_time() == 10.0
+	assert l1 <= l2
+	queue.update(10000.0)
+	l3 = queue.get_len()
+	assert queue.get_time() == 10000.0
+	assert l2 <= l3
