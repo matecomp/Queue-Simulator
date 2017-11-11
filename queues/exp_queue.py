5af3a359f391379e86c25f37d29ed139ff1979e3
Mateus Ildefonso <matecompufrj@gmail.com>
2017-11-11 16:25:28 -0200

    Add exp_queue on project

--- queues/exp_queue.py
+++ queues/exp_queue.py
@@ -3,12 +3,41 @@ import numpy as np
 import collections as clt
 
 
-class FCFS(object):
-	"""docstring for FCFS"""
-	def __init__(self, servers=list()):
-		super(FCFS, self).__init__()
-		self.servers = servers
-		self.queue = clt.deque()
+class Queue(object):
+	"""docstring for Queue"""
+	def __init__(self, lamb=1.0):
+		super(Queue, self).__init__()
+		self.__params = self.make_params_dict(lamb, clt.deque())
 
-	def add_client(self, time_client):
-		self.queue.append(time_client)
+	def make_params_dict(self, lamb, deque):
+		return dict(zip(
+			["lambda", "beta", "deque"],
+			[lamb, 1/lamb, deque]
+		))
+
+	def get_params(self):
+		return self.__params
+
+	def get_len(self):
+		return len(self.__params["deque"])
+
+	def add_client(self, size=None):
+		time_client = np.random.exponential(scale=self.__params["beta"], size=size)
+		if size is None:
+			self.__params["deque"].append(time_client)
+		else:
+			self.__params["deque"].extend(time_client)
+
+	def pop_fcfs(self):
+		if len(self.__params["deque"]) == 0:
+			return None
+		return self.__params["deque"].popleft()
+
+	def pop_lcfs(self):
+		if len(self.__params["deque"]) == 0:
+			return None
+		return self.__params["deque"].pop()
+
+
+if __name__ == '__main__':
+	print("Ops...")
