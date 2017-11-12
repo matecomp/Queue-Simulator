ac7b4a9c8788663ee5edc4a781986a8af9b6585f
Mateus Ildefonso <matecompufrj@gmail.com>
2017-11-12 02:35:58 -0200

    Create a simple MM1 test

--- main.py
+++ main.py
@@ -7,35 +7,59 @@ import collections as clt
 from queues.exp_queue import Queue
 from servers.exp_server import Server
 
-time = 0
-queue = Queue()
-server = Server(mu=100)
 
-
-if __name__ == '__main__':
-
-	service_time = 0.0
+def run_system(queue, server, epochs, time=0.0):
+	busy = 0.0
+	idle = 0.0
 	queue_size = list()
-	#while(1):
-	for i in range(100000):
-		# Se o servidor estiver vazio, o cliente sairá 
-		# instantaneamente da fila
-		# Se o servidor estiver ocupado, vamos progredir o tempo
-		# de serviço e atualizar o sistema após o tempo de serviço
+	est_busy = 0.0
+
+	for idx in range(epochs):
 		if server.is_empty() == True:
 			client_time = queue.pop_fcfs()
-			if client_time is None:
+			if client_time == None:
 				queue.add_client()
 				client_time = queue.pop_fcfs()
-				service_time = server.do_service(client_time + time)
+				idle += client_time
+				time += client_time
+				busy = server.do_service(time)
+				time += busy
+				est_busy += busy
 			else:
-				service_time = server.do_service(time)
-		else:
-			time += service_time
-			server.update(time)
-			queue.update(time)
+				busy = server.do_service(time)
+				time += busy
+				est_busy += busy
+		server.update(time)
+		queue.update(time)
+		if idx%1 == 0:
 			queue_size.append(queue.get_len())
 
 	plt.plot(range(len(queue_size)), queue_size)
 	plt.show()
-	print(queue_size[-1])
+	ro = est_busy/time
+	return ro
+
+
+def simulateMM1(number_it=100000, lamb=1.0, mu=1.0):
+	queue = Queue(lamb=lamb)
+	server = Server(mu=mu)
+	ro = run_system(queue, server, number_it, 0.0)
+	return ro
+
+def MM1(lamb=1.0, mu=1.0):
+	ro = lamb/mu
+	X = 1/mu
+	Nq = ro/(1-ro)
+	W = Nq*lamb
+	return dict(zip(
+			["W", "Nq", "X", "ro"],
+			[W, Nq, X, ro]
+		))
+
+if __name__ == '__main__':
+	lamb = 1.0
+	mu = 1.001
+	number_it = 100000
+	ro = simulateMM1(number_it, lamb, mu)
+	params = MM1(lamb, mu)
+	print(ro, params["ro"])
